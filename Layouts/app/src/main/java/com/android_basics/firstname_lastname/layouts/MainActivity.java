228468863ac602830fb54ae8f818555f89e00b13
Dimitrii Home <dimitrii@engineering-bear.com>
2017-06-13 21:08:42 -0700

    Added Activity & Layout

---
+++ Layouts/app/src/main/java/com/android_basics/firstname_lastname/layouts/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android_basics.firstname_lastname.layouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
