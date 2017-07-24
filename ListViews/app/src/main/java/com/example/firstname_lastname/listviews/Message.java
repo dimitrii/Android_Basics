c1c4c6de0660222be2392bb913852b655af97f6a
Dimitrii Pokrovskii <dimitrii@engineering-bear.com>
2017-07-24 12:59:07 -0700

    Added ListView Example

---
+++ ListViews/app/src/main/java/com/example/firstname_lastname/listviews/Message.java
@@ -0,0 +1,10 @@
+package com.example.firstname_lastname.listviews;
+
+/**
+ * Created by Dimitrii_Pokrovskii on 7/24/2017.
+ */
+
+public class Message {
+    public String message;
+    public String sender;
+}
