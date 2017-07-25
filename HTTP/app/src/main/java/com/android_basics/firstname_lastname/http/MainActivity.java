0db736aaa20f115f857b14ecdd5c77db209911f8
Dimitrii Pokrovskii <dimitrii@engineering-bear.com>
2017-07-25 13:01:50 -0700

    Cleaning up HTTP

--- HTTP/app/src/main/java/com/android_basics/firstname_lastname/http/MainActivity.java
+++ HTTP/app/src/main/java/com/android_basics/firstname_lastname/http/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 import okhttp3.OkHttpClient;
@@ -28,10 +31,28 @@ public class MainActivity extends Activity {
             }
         }
         protected void onPostExecute(String response) {
+
             // Write Text
             TextView mainText = (TextView) findViewById(R.id.mainTextView);
-            if (mainText != null && response != null) {
-                mainText.setText(response);
+//            if (mainText != null && response != null) {
+//                mainText.setText(response);
+//            }
+
+            String jsonData = response;
+            @com.sun.istack.internal.NotNull JSONArray list;
+            try {
+                list = new JSONArray(jsonData);
+            } catch (Exception e) {
+                // Do nothing
+            } finally {
+                for(int i = 0; i < list.length(); i++) {
+                    JSONObject obj;
+                    try {
+                        obj = list.getJSONObject(i);
+                    } catch (Exception e) {} finally {
+                        mainText.append(obj.getString("public_url"));
+                    }
+                }
             }
         }
     }
@@ -40,7 +61,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        new RetrieveUrlTask().execute("https://raw.github.com/square/okhttp/master/README.md");
+        new RetrieveUrlTask().execute("https://storybox-145021.appspot.com/api/audio/featured");
     }
 }
